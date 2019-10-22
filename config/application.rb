8e5aa15e0e7f73d76ca5cabf265037621dfa5b2d
maxrosales83 <maxrosales83@gmail.com>
2019-10-22 21:22:39 +0000

    compiler

--- config/application.rb
+++ config/application.rb
@@ -12,8 +12,7 @@ module JobBoard
     config.application_name = 'Job Board'
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
-# config/application.rb
-config.assets.initialize_on_precompile = false
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
