1e08fd1e5da117c83f97406a29adfc8df2c346f5
maxrosales83 <maxrosales83@gmail.com>
2019-10-27 23:21:58 +0000

    api key again

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,5 +3,5 @@ Rails.configuration.stripe = {
   :secret_key      => ENV['stripe_api_key']
 }
 
-Stripe.api_key = ENV['stripe_publishable_key']
+Stripe.api_key = ENV['stripe_api_key']
 #Stripe.api_key = Rails.configuration.stripe[:publishable_key]
