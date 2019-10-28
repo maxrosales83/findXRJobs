f43630479e2aae4b4894c0b8575995fadd23e660
maxrosales83 <maxrosales83@gmail.com>
2019-10-28 00:43:37 +0000

    change charge in jobs controller

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -39,7 +39,7 @@ class JobsController < ApplicationController
     card_last4 = params[:user][:card_last4]
 
     charge = Stripe::Charge.create(
-      :amount => 30000,
+      :amount => 100,
       :currency => "usd",
       :description => job_type,
       :statement_descriptor => job_title,
