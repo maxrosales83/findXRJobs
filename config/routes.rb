93d5b81689104a28cca1ee8cfe771c84256b955d
maxrosales83 <maxrosales83@gmail.com>
2019-10-31 01:17:11 +0000

    add header, styling, and info page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   resources :jobs
   devise_for :users
   root to: 'jobs#index'
-
+  get '/info' => 'pages#info'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
