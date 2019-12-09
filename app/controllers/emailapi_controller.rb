e5c377c3ba7b38df451fd80a7b40848852175410
maxrosales83 <maxrosales83@gmail.com>
2019-12-09 19:45:48 +0000

    added subscribed error to sign up form

--- app/controllers/emailapi_controller.rb
+++ app/controllers/emailapi_controller.rb
@@ -1,18 +1,22 @@
 class EmailapiController < ApplicationController
  def subscribe
-   @list_id = ENV["MAILCHIMP_LIST_ID"]
-  gibbon = Gibbon::Request.new
+  @list_id = ENV["MAILCHIMP_LIST_ID"]
+    gibbon = Gibbon::Request.new
+  
+    gibbon.lists(@list_id).members.create(
+      body: {
+        email_address: params[:email][:address],
+        status: "subscribed"
+      }
+    )
 
-  gibbon.lists(@list_id).members.create(
-    body: {
-      email_address: params[:email][:address],
-      status: "subscribed"
-    }
-  )
-
-  respond_to do |format|
-  format.json{render :json => {:message => "You have been Successfully added to the list! :)"}}
-    end
+    
+    respond_to do |format|
+    format.html {redirect_to root_path}
+    flash[:alert] = "Subscribed!"
+    flash.keep(:alert) # Keep flash notice around for the redirect.
+    format.js {render :js => "window.location.href='"+root_path+"'"} 
+  end
  end
 end
 
