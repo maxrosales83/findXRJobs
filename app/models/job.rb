6630f99b4f24b307dd6bdaf12490546b438047a1
maxrosales83 <maxrosales83@gmail.com>
2019-12-26 05:47:07 +0000

    search form styling

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,7 +5,7 @@ class Job < ApplicationRecord
   JOB_TYPES = ["Full-time", "Part-time", "Contract", "Freelance"]
   
   def self.search(params)
-    jobs = Job.where("body LIKE ? or title LIKE ?"  "%#{params[:search]}%",
+    jobs = Job.where("description LIKE ? or title LIKE ? or job_type LIKE ? or location LIKE ?", "%#{params[:search]}%", "%#{params[:search]}%", "%#{params[:search]}%",
            "%#{params[:search]}%") if params[:search].present?
     jobs
   end
