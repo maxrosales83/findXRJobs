440adc994a46a2565c1614db72b45f76443437ab
maxrosales83 <maxrosales83@gmail.com>
2020-01-06 20:10:27 +0000

    search authentication fix

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,6 +1,6 @@
 class JobsController < ApplicationController
   before_action :set_job, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, except: [:index, :show]
+  before_action :authenticate_user!, except: [:index, :show, :search]
   # GET /jobs
   # GET /jobs.json
   def index
